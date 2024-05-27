59d106a7bf07b0f4f0322043ba4df02a2d9ca3ed
julianamedeiros <juliana.652@gmail.com>
2024-05-27 17:31:54 +0200

    do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from data_pipeline.extraction import ext
 import pandas as pd
-import sqlalchemy
+from sqlalchemy import *
 
 
 #Defining endpoints and extracting
@@ -8,7 +8,7 @@ raw_games = ext('games')
 raw_genre = ext('genres')
 
 
-#model and process data
+# PROCESSING DATA
 #filter the columns to the ones that match your model, drop duplicates and check null values
 
 cleaned_games = raw_games[['id', 'name', 'first_release_date', 'genres']]
@@ -24,8 +24,50 @@ cleaned_genre = cleaned_genre.sort_values(by='id')
 #joining both tables
 
 joined_tb = pd.merge(cleaned_games, cleaned_genre, how='left', left_on='genres', right_on='id')
+
+#adjusting data types
 joined_tb = joined_tb.drop(columns=['genres', 'id_y'])
 joined_tb = joined_tb.rename(columns={'name_y':'genre'})
+joined_tb = joined_tb.rename(columns={'id_x':'game_id'})
+joined_tb = joined_tb.rename(columns={'name_x':'name'})
+joined_tb['game_id'] = joined_tb['game_id'].astype(int)
+joined_tb['name'] = joined_tb['name'].astype(str)
+joined_tb['genre'] = joined_tb['genre'].astype(str)
 
-#load to database
 
+# LOADING TO DATABASE
+
+engine = create_engine('postgresql://postgres:password@localhost/games')
+
+def load_to_staging(df):
+    df.to_sql('staging_games', engine, if_exists='replace', index=False)
+    print('Data loaded into staging table')
+
+
+load_to_staging(joined_tb)
+
+
+#joining staging with main table:
+join_query = text('''
+INSERT INTO games (game_id, name, first_release_date, genre)
+SELECT s.game_id, s.name, s.first_release_date, s.genre
+FROM staging_games s
+LEFT JOIN games g 
+ON s.game_id = g.game_id 
+AND s.name = g.name 
+AND s.first_release_date = g.first_release_date 
+AND s.genre = g.genre
+WHERE g.game_id IS NULL 
+AND g.name IS NULL 
+AND g.first_release_date IS NULL 
+AND g.genre IS NULL;''')
+
+with engine.connect() as conn:
+    conn.execute(join_query)
+    print('Data merged into main table')
+
+# Cleaning staging table
+truncate_query = text("TRUNCATE TABLE staging_games;")
+with engine.connect() as conn:
+    conn.execute(truncate_query)
+    print('Staging table truncated')
